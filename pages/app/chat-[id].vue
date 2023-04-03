50959930baff8c71d8e87a1b23f17cced70e18e1
Henriette Einstein <45788168+henriette-einstein@users.noreply.github.com>
2023-04-03 13:46:28 +0200

    Simplified chat

--- pages/app/chat-[id].vue
+++ pages/app/chat-[id].vue
@@ -1,23 +1,20 @@
 <template>
-  <div class="flex flex-col h-full w-full prose max-w-none">
-    <h1>Have a Chat with {{ person.title }}</h1>
-    <div class="bg-base-200 h-4/5 p-2 overflow-y-auto">
-      <div v-for="(entry, index) in chat" :key="index" :class="entry.q ? 'chat chat-start' : 'chat chat-end'">
-        <div
-          :class="entry.q ? 'chat-bubble text-primary bg-primary-content' : 'chat-bubble text-secondary bg-secondary-content'">
-          {{ entry.message }}</div>
-      </div>
+  <div class="grid grid-rows-[70vh,25vh]">
+    <div ref="historyContainer" class="overflow-y-auto">
+      <div v-for="(entry, index) in history" :key="index" :class="getMessageClass(entry)">
+          <p>{{ entry.message }}</p>
+        </div>
     </div>
-    <div class="flex flex-1 flex-col bg-base-100 h-1/4 justify-center items-center py-3 border shadow-xl">
-      <div class="tabs mb-2">
+    <div class="border overflow-y-auto">
+      <div class="tabs h-1/5 m-0 justify-center">
         <a :class="activeTab === 'chat' ? 'tab tab-bordered tab-active' : 'tab tab-bordered'" href="#"
           v-on:click.prevent="activeTab = 'chat'">Chat</a>
         <a :class="activeTab === 'settings' ? 'tab tab-bordered tab-active' : 'tab tab-bordered'" href="#"
           @click.prevent="activeTab = 'settings'">Settings</a>
       </div>
-      <textarea v-if="activeTab === 'chat'" class="h-full w-full p-2 focus:outline-none"
+      <textarea v-if="activeTab === 'chat'" class="h-4/5 w-full p-2 focus:outline-none"
         placeholder="Type your message here" v-model="question" @keydown.enter="addQuestion"></textarea>
-      <form v-if="activeTab === 'settings'" class="h-full w-full p-2 bg-base-200">
+      <form v-if="activeTab === 'settings'" class="h-4/5 w-full p-2 bg-base-200">
         <div class="grid grid-cols-4">
           <label for="temp" class="label">Temperature: </label>
           <input id="temp" type="text" class="input input-bordered w-full col-span-3" v-model="temperature"
@@ -30,6 +27,7 @@
       </form>
     </div>
   </div>
+
 </template>
 
 <script setup>
@@ -46,15 +44,18 @@ const id = route.params.id;
 
 const person = sherlock.config.people[id]
 
+
 /** Input fields */
 const activeTab = ref("chat");
 const question = ref("");
 const temperature = ref(0.7);
 const prompt = ref(0)
 
+const historyContainer =ref(null)
+
 const chatModel = await sherlock.newChat(temperature.value)
 
-const systemMessage = SystemMessagePromptTemplate.fromTemplate("You are a history professor with a lot of knowledge about Sherlock Holmes. Answer all questions and give a good explanation.")
+const systemMessage = SystemMessagePromptTemplate.fromTemplate(person.prompt)
 const humanMessage = HumanMessagePromptTemplate.fromTemplate("{question}")
 const promptTemplate = ChatPromptTemplate.fromPromptMessages([systemMessage, humanMessage])
 
@@ -70,30 +71,41 @@ const prompts = [
   { id: 2, label: "Template 2" },
   { id: 3, label: "Template 3" },
 ]
-let chat = [
+let history = ref([
   {
     message: person.intro,
     q: false,
   }
-];
-
+])
 async function addQuestion() {
 
   const answer = await chain.call({ question: question.value })
 
-  // alert(JSON.stringify(answer))
-  // console.log(answer)
-  //const answer = ref("This is a test answer")
-  chat.push({
+  history.value.push({
     message: question.value,
     q: true,
   })
-  chat.push({
+  history.value.push({
     message: answer.response,
     q: false,
   })
+  scrollToLastMessage()
   question.value = "";
 };
+
+function scrollToLastMessage() {
+  if (historyContainer.value) {
+    historyContainer.value.scrollTop = historyContainer.value.scrollHeight
+  }
+}
+
+function getMessageClass(entry) {
+  if (entry.q) {
+    return 'text-red-500'
+  } else {
+    return 'text-blue-500'
+  }
+}
 
 
 </script>
