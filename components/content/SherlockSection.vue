115d999b73bf6c921c97049ff609a93628b898e4
Henriette Einstein <45788168+henriette-einstein@users.noreply.github.com>
2023-04-06 11:49:17 +0200

    Added SherlockSection component

---
+++ components/content/SherlockSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between cursor-pointer prose" @click="isExpanded = !isExpanded">
+      <p v-if="headinglevel == 0" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</p>
+      <h1 v-if="headinglevel == 1" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h1>
+      <h2 v-if="headinglevel == 2" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h2>
+      <h3 v-if="headinglevel == 3" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h3>
+      <h4 v-if="headinglevel == 4" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h4>
+      <h5 v-if="headinglevel == 5" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h5>
+      <h6 v-if="headinglevel == 6" class="flex items-center"><up-down class="mr-2" :class="{ 'rotate-180': isExpanded }"/>{{ title }}</h6>
+    </div>
+    <transition name="fade">
+      <div v-show="isExpanded">
+        <ContentSlot :use="$slots.default" unwrap="div" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script setup>
+const isExpanded = ref(true)
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Titel fehlt'
+  },
+  headinglevel: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.6s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- content/quellen.md
+++ content/quellen.md
@@ -5,6 +5,7 @@
 - [Projekt Gutenberg englisch](https://www.gutenberg.org/ebooks/search/?query=conan+doyle&submit_search=Go%21)
 - [Projekt Gutenberg deutsch](https://www.projekt-gutenberg.org/autoren/namen/doyle.html)
 
-## Sontiges
+::sherlock-section{title="Sonstiges" headinglevel=2}
 - [Strand Magazin auf Wikipedia](https://de.wikipedia.org/wiki/The_Strand_Magazine)
 - [Strand Magazin](https://strandmag.com) - Webseite des aktuellen Magazins
+::
